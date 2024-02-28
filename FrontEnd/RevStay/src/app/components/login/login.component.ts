05f54968fd2ecf0430711b23b3e4e050c8c5a27e
ZachMeininger <meiningerzach@gmail.com>
2024-02-28 13:55:36 -0500

    Login component working

--- FrontEnd/RevStay/src/app/components/login/login.component.ts
+++ FrontEnd/RevStay/src/app/components/login/login.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  userId: Number = 0;
+  userEmail: String = '';
+  userPassword: String = '';
+  securityQuestion: String = 'a';
+  securityAnswer: String  = 'b';
+  accountType: Number = 0;
+
+  handleLogin() {
+    const UserAccount = {
+      userId: this.userId,
+      userEmail: this.userEmail,
+      userPassword: this.userPassword,
+      securityQuestion: this.securityQuestion,
+      securityAnswer: this.securityAnswer,
+      accountType: this.accountType
+    };
+
+    console.log(UserAccount);
+
+    if((UserAccount.userEmail).length == 0 || (UserAccount.userPassword).length == 0)
+    {
+
+    }
+    else
+    {
+        fetchData();
+    }
+    
+    async function fetchData() {
+
+        let response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        body: JSON.stringify(UserAccount),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+
+        if(response.status >= 400)
+        {
+
+        }
+        else
+        {
+            let data = await response.json();
+            console.log(data);
+        }
+    }
+  }
 }
