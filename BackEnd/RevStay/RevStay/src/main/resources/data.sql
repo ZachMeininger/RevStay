85bc5cf37e9257b905c99ef682e9ea2dde988e72
HyperionPanda <aidanghorn1@gmail.com>
2024-03-12 20:11:24 -0700

    structure changes to home and view hotel

--- BackEnd/RevStay/RevStay/src/main/resources/data.sql
+++ BackEnd/RevStay/RevStay/src/main/resources/data.sql
@@ -105,10 +105,10 @@ CREATE TABLE Customer_Invoice(
 
 INSERT INTO User_Account(User_Email, User_Password, Security_Question, Security_Answer, Account_Type) VALUES ('email','password','sq','sa',1);
 INSERT INTO Customer_Account(Customer_First, Customer_Last, User_Id) VALUES ('Bob','Barker',1);
-INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('name1','address1','description1','image1',1000,0,0,0,0,1);
-INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('name2','address2','description2','image2',2000,1000,0,0,0,1);
-INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('name3','address3','description3','image3',3000,2000,0,0,0,1);
-INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('name4','address4','description4','image4',4000,3000,0,0,0,1);
+INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('Hotel California','Los Angeles','A hotel located in the heart of California. Thats all there is too it','image1',1000,0,0,0,0,1);
+INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('Four Points Hotel and Diner','Pleasanton','A hotel with in door dining and actual beds!','image2',2000,1000,0,1,1,1);
+INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('Corner of the World','Charleston','Some corner place. Like its literally in a corner','image3',3000,2000,0,1,0,1);
+INSERT INTO Hotel_Account(Hotel_Name, Hotel_Address, Hotel_Description, Hotel_Image,Price_High,Price_Low, Hotel_Pool, Hotel_Sauna, Hotel_Rest, User_Id) VALUES ('Abyss','Abyss','Some reallly dark place. Lighting needs to be paid for','image4',4000,3000,1,0,0,1);
 
 INSERT INTO Customer_Review (Review_Star, Review_Body, Hotel_Id, Customer_Id) VALUES (4,'first review',2,1);
 INSERT INTO Customer_Review (Review_Star, Review_Body, Hotel_Id, Customer_Id) VALUES (2,'second review',2,1);
