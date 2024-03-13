ee6dfeaaa708e1f73873e5420fee89098d644499
ZachMeininger <meiningerzach@gmail.com>
2024-03-13 16:40:43 -0400

    checkout input error handling

--- FrontEnd/RevStay/src/app/components/checkout/checkout.component.ts
+++ FrontEnd/RevStay/src/app/components/checkout/checkout.component.ts
@@ -14,7 +14,38 @@ export class CheckoutComponent {
 
   constructor(private router: Router) {}
 
+  cardName: String = '';
+  cardNum: String = '';
+  cardExp: String = '';
+  cardCvv: String = ''
+
+  nameError: Number = 0;
+  numError: Number = 0;
+  expError: Number = 0;
+  cvvError: Number = 0;
+
   handleCheckout() {
-    this.router.navigate(['/home']);
+    if((this.cardName).length < 3)
+    {
+      this.nameError = 1;
+    }
+    else if((this.cardNum).length != 16)
+    {
+      this.numError = 1;
+    }
+    else if((this.cardExp).length != 5)
+    {
+      this.expError = 1;
+    }
+    else if((this.cardCvv).length != 3)
+    {
+      this.cvvError = 1;
+    }
+    else
+    {
+      this.router.navigate(['/home']);
+    }
+
+    
   }
 }
